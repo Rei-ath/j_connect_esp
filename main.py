cdab11cd1dc517bdc4b1f61c5af0e57f24c9e708
Rei-ath <hk35456@gmail.com>
2023-10-31 05:16:42 +0530

    minor changes

--- main.py
+++ main.py
@@ -6,29 +6,37 @@ from motor import Motor
 
 motor1 = Motor(pin_a=2, pin_b=4, enable_pin=5)
 motor2 = Motor(pin_a=13, pin_b=12, enable_pin=14)
+motor1.set_speed_in_percentage(100)
+
+
+# motor2.set_pwm_frequency(40000)
+motor2.set_speed_in_percentage(100)
 async def vroom():
     while True:
         print("VROOOOOMMM...")
         await uasyncio.sleep(6)
 
 async def read_socket_data(soc: socket.socket):
-    print_loop = uasyncio.get_event_loop()
-    print_loop.create_task(vroom())
+    # print_loop = uasyncio.get_event_loop()
+    # print_loop.create_task(vroom())
     while True:
         try:
             data = soc.recv(10)
             # soc.send(bytes()"lol")
             # soc.send(b"lol")
-            print("data is ",data)
-            print(len(data),"data lenght")
+            while not data:
+                print("vroom")
+
             if len(data) == 1:
+                print("data is ",data)
+                print(len(data),"data lenght")
                 event = int.from_bytes(data, 'big')
                 print(data)
                 if event == MOTOR_ON_EVENT:
                     print("Moving ig")
-                    await motor1.set_pwm_frequency(40000)
+                    # await motor1.set_pwm_frequency(40000)
                     await motor1.set_speed_in_percentage(100)
-                    await motor2.set_pwm_frequency(40000)
+                    # await motor2.set_pwm_frequency(40000)
                     await motor2.set_speed_in_percentage(100)
                 elif event == MOTOR_OFF_EVENT:
                     print("Stopping motors")
@@ -49,12 +57,12 @@ async def read_socket_data(soc: socket.socket):
                 elif event == MOTOR_RIGHT_EVENT:
                     print("Moving right")
                     await motor2.move_backward()
-                    time.sleep(0.3)
+                    time.sleep(0.01)
                     await motor2.move_forward()
                 elif event == MOTOR_LEFT_EVENT:
                     print("Moving left")
                     await motor1.move_backward()
-                    time.sleep(0.3)
+                    time.sleep(0.01)
                     await motor1.move_forward()
             # soc.send(bytes("heollo", "utf-8"))
             print("Going to sleep for 1 second")
